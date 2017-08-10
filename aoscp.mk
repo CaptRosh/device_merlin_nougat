7d4f12e2049b558c0708698bfb36d15e6a041949
Roshan Srivastava <roshan.srivastava01@gmail.com>
2017-08-10 19:19:22 +0530

    Update aoscp.mk

--- aoscp.mk
+++ aoscp.mk
@@ -1,5 +1,5 @@
-# Copyright (C) 2015 The CyanogenMod Project
-#           (C) 2017 The LineageOS Project
+#
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,17 +12,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 $(call inherit-product, device/motorola/merlin/full_merlin.mk)
 
-# Boot animation
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
+# Inherit some common aoscp stuff.
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := merlin
 PRODUCT_NAME := aoscp_merlin
-PRODUCT_MODEL := MotoG3-TE
-PRODUCT_BRAND := Motorola
-PRODUCT_MANUFACTURER := Motorola
-PRODUCT_RELEASE_NAME := merlin
+BOARD_VENDOR := Motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
